5df669fb666e1939d62a7965d5112d46382b34f9
Marvin <fuxxel66@gmail.com>
2019-12-12 21:34:32 +0100

    Update

--- visualize_dtw.py
+++ visualize_dtw.py
@@ -2,29 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os 
 
-input_path = "dtw/Lager4/skip_29"
-save = "dtw_lager4_skip_29.png"
+input_path = "dtw/Lager5/skip_29"
+save = "dtw_lager5_skip_29_around_error.png"
 log = False
-cutoff_index = 323
+cutoff_start = 2167 - (29*12)
+cutoff_end = 2167 + (29*12)
+
+# cutoff_index = 323
+downsample = 1
 
 files = os.listdir(input_path)
 files = filter(lambda x: x.endswith(".dtw"), files)
 files = list(sorted(files))
 
 dates = [".".join(x.split("_")[1:3][::-1]) for x in files]
-ticks = [y + ". " + ":".join(x.split("_")[-3:])[:-4] for x,y in zip(files, dates)][:cutoff_index]
+ticks = [y + ". " + ":".join(x.split("_")[-3:])[:-4] for x,y in zip(files, dates)][cutoff_start:cutoff_end:downsample]
 
 files = [os.path.join(input_path, x) for x in files]
 
 dtw_data = []
-for file in files[:cutoff_index]:
+for file in files[cutoff_start:cutoff_end:downsample]:
 	data = np.loadtxt(file)
 	dtw_data.append(data)
 
 dtw_data = np.array(dtw_data)
 
-plt.figure(figsize=(11.27*7, 7.04), dpi=227)
-plt.title("DTW (2019_02_28__12_04_16.mat)")
+plt.figure(figsize=(11.27*12, 7.04), dpi=227)
+plt.title("DTW (2019_03_13__14_03_16.mat)")
 if log:
 	plt.semilogy(dtw_data, label="Global error")
 else:
